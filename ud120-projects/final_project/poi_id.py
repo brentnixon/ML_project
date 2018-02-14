68dbea9da47f9103b794ed32adf592696f4aad60
Brent Nixon <nixonalanbrent@gmail.com>
2018-02-13 19:02:53 -0800

    add cross validation

--- ud120-projects/final_project/poi_id.py
+++ ud120-projects/final_project/poi_id.py
@@ -1,23 +1,55 @@
 #!/usr/bin/python
 
 import sys
+import os
 import pickle
+from pprint import pprint
+
+os.chdir('/Users/brentan/Documents/DAND/Projects/ML/ud120-projects/final_project')
 
 sys.path.append("../tools/")
-
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
+
 ### Task 1: Select what features you'll use.
-### features_list is a list of strings, each of which is a feature name.
-### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "rb") as data_file:
     data_dict = pickle.load(data_file)
+    
+# show list of possible features
+feature_names = list(list(data_dict.values())[0].keys())
+pprint(feature_names)
+
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+features_list = ['poi',   #kept all features but email_address (text)
+                 'salary',
+                 'to_messages',
+                 'deferral_payments',
+                 'total_payments',
+                 'loan_advances',
+                 'bonus',
+                 'restricted_stock_deferred',
+                 'deferred_income',
+                 'total_stock_value',
+                 'expenses',
+                 'from_poi_to_this_person',
+                 'exercised_stock_options',
+                 'from_messages',
+                 'other',
+                 'from_this_person_to_poi',
+                 'poi',
+                 'long_term_incentive',
+                 'shared_receipt_with_poi',
+                 'restricted_stock',
+                 'director_fees']
 
 ### Task 2: Remove outliers
+data_dict.pop('TOTAL')
+
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -26,7 +58,11 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
-### Task 4: Try a varity of classifiers
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
+### Task 4: Try a variety of classifiers
 ### Please name your classifier clf for easy export below.
 ### Note that if you want to do PCA or other multi-stage operations,
 ### you'll need to use Pipelines. For more info:3
@@ -44,11 +80,8 @@ clf = GaussianNB()
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
-np.array([0])
-np.array([0])
+
+
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
